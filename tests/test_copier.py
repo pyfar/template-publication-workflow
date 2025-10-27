7ac52e5310aa91453adffe506c77fd5c0fb4f0e3
Anne Heimes <ahe@akustik.rwth-aachen.de>
2025-10-27 20:16:51 +0100

    add licensing

--- tests/test_copier.py
+++ tests/test_copier.py
@@ -9,8 +9,9 @@ import pytest
 
 IS_WINDOWS = platform.startswith('win')
 
-@pytest.fixture
-def defaults():
+
+@pytest.fixture(scope='session')
+def copier_project_defaults():
     return {
         "project_name": "my_project",
         "package_description": "my_project",
@@ -19,12 +20,40 @@ def defaults():
         "python_version": "3.10",
         "license": "MIT",
         "username": "pyfar",
-        "publication_slug": "year_where_shorttitle",
         "version": "0.1.0",
-    }
+        "year": "2024",
+        }
 
-def test_copier_defaults(tmpdir, defaults):
-    run_copy('.', tmpdir, defaults)
-    assert (tmpdir / 'pyproject.toml').exists()
+def test_project_folder(copie, copier_project_defaults):
+    project_defaults = copier_project_defaults
+    project = copie.copy(extra_answers=project_defaults)
 
+    assert project.exit_code == 0
+    assert project.exception is None
+    assert project.project_dir.is_dir()
+
+
+def test_readme(copie, copier_project_defaults):
+    # create project
+    project_defaults = copier_project_defaults
+    project = copie.copy(extra_answers=project_defaults)
+
+    # test existence
+    assert os.path.exists(os.path.join(project.project_dir, "README.md"))
+
+    # test content
+    content = project.project_dir.joinpath("README.md").read_text()
+    assert '# Welcome to my_project' in content
+
+
+def test_license_default(copie, copier_project_defaults):
+    # create project
+    project_defaults = copier_project_defaults
+    project = copie.copy(extra_answers=project_defaults)
+
+    # test LICENSE file
+    assert os.path.exists(os.path.join(project.project_dir, "LICENSE"))
+    content = project.project_dir.joinpath("LICENSE").read_text()
+    assert 'MIT License' in content
+    assert '2024 author' in content
 
